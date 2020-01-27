c53f64f1d88a900dfad2572608d2e864a7348ba0
Nikita <spionop@yandex.ru>
2020-01-27 18:15:23 +0000

    Initial

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git config user.name "Nikita"
+git config user.email "spionop@yandex.ru"
+unlink /etc/nginx/sites-enabled/default
+ln -s etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+/etc/init.d/nginx restart
+echo "Complete!"
