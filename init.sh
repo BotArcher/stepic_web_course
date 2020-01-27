125928b1e227ce069f961e4f99d59e353a6ae08e
Nikita <35061723+BotArcher@users.noreply.github.com>
2020-01-27 23:24:58 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@
 git config user.name "Nikita"
 git config user.email "spionop@yandex.ru"
 unlink /etc/nginx/sites-enabled/default
-ln -s etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 /etc/init.d/nginx restart
 echo "Complete!"
