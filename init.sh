1bbc0247da4ea2d9b4095399c4cd621a0cc4d1b8
Nikita <spionop@yandex.ru>
2020-01-28 10:38:11 +0000

    Nginx is running!

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 
 git config user.name "Nikita"
 git config user.email "spionop@yandex.ru"
+mv  /home/box/test1 /home/box/web
 unlink /etc/nginx/sites-enabled/default
 ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 /etc/init.d/nginx restart
