ff95e93d5550ecc7a6a3b6d2b3f5ee554ab6f459
Nikita <35061723+BotArcher@users.noreply.github.com>
2020-01-28 19:10:48 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ git config user.name "Nikita"
 git config user.email "spionop@yandex.ru"
 mv  /home/box/test1 /home/box/web
 unlink /etc/nginx/sites-enabled/default
+ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test.conf
 ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 /etc/init.d/nginx restart
 echo "Complete!"
