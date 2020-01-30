07000a0d91a1b5ed2d4ab8fc06b9fd50987fcfa4
Nikita <spionop@yandex.ru>
2020-01-30 16:17:08 +0000

    No comments...

--- init.sh
+++ init.sh
@@ -4,7 +4,8 @@ git config user.name "Nikita"
 git config user.email "spionop@yandex.ru"
 mv  /home/box/test1 /home/box/web
 unlink /etc/nginx/sites-enabled/default
-ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test.conf
 ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test.conf
 /etc/init.d/nginx restart
+gunicorn -c /etc/gunicorn.d/test.conf hello &
 echo "Complete!"
