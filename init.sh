e811d014ea3282bf5d79149d3badbd96bf1db017
Nikita <35061723+BotArcher@users.noreply.github.com>
2020-01-30 21:26:12 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,4 +8,5 @@ ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test.conf
 /etc/init.d/nginx restart
 gunicorn -c /etc/gunicorn.d/test.conf hello &
+gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi &
 echo "Complete!"
